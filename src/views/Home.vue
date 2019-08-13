bb14ba4e6743489bc6f6f166df8a5dd15fff68ea
Victor Rocha <vrsarmento@gmail.com>
2019-08-13 18:36:11 -0300

    Header and Footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,30 @@
 <template>
-  <div>
+  <section>
+    <h1>Compre ou Venda</h1>
 
-  </div>
+    <ProductsSearch/>
+    <ProductsList/>
+  </section>
 </template>
 
 <script>
+import ProductsSearch from "@/components/ProductsSearch.vue";
+import ProductsList from "@/components/ProductsList.vue";
+
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    ProductsSearch,
+    ProductsList
+  }
 }
 </script>
+
+<style scoped>
+h1 {
+  color: #87f;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
