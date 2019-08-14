f3171a2d8e19dfdd9ad1d675c1b3685f0c35f956
Victor Rocha <vrsarmento@gmail.com>
2019-08-14 19:58:05 -0300

    Products fetch and API

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,12 +1,33 @@
 <template>
-	<div>
-		
-	</div>
+	<section class="products-container">
+		<div v-for="product in products" :key="product.id">
+			<img v-if="product.photos" :src="product.photos[0].src" :alt="product.photos[0].title">
+			<p class="price">{{product.price}}</p>
+			<h2 class="title">{{product.name}}</h2>
+			<p>{{product.description}}</p>
+		</div>
+	</section>
 </template>
 
 <script>
 export default {
-
+	data() {
+		return {
+			products: null
+		}
+	},
+	methods: {
+		getProducts() {
+			fetch("http://localhost:3000/product")
+				.then(r => r.json())
+				.then(r => {
+					this.products = r;
+				})
+		}
+	},
+	created() {
+		this.getProducts();
+	}
 }
 </script>
 
