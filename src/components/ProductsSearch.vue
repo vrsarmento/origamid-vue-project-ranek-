7e6a90c85238ee3691b7fb547e2f67d578fe01eb
Victor Rocha <vrsarmento@gmail.com>
2019-08-15 19:00:59 -0300

    Home page style

--- src/components/ProductsSearch.vue
+++ src/components/ProductsSearch.vue
@@ -1,6 +1,6 @@
 <template>
 	<form>
-		<input type="text" name="search" id="search" v-model="search">
+		<input type="text" name="search" id="search" v-model="search" placeholder="Buscar...">
 		<input type="submit" id="magnifier" value="Buscar" @click.prevent="searchProducts">
 	</form>
 </template>
@@ -22,6 +22,35 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+form {
+	max-width: 600px;
+	margin: 30px auto 60px auto;
+	position: relative;
+}
+
+#search {
+	width: 100%;
+	padding: 20px;
+	border: none;
+}
+
+#search:focus,
+#search:hover {
+	transform: scale(1.1);
+}
+
+#magnifier {
+	width: 62px;
+	height: 62px;
+	background: url("../assets/search.svg") no-repeat center center;
+	text-indent: -150px;
+	border: none;
+	cursor: pointer;
+	position: absolute;
+	top: 0px;
+	right: 0px;
+	box-shadow: none;
+}
 
 </style>
