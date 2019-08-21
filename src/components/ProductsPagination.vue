3c8c4b21b26b908d497890c52a3b9db98bcfb615
Victor Rocha <vrsarmento@gmail.com>
2019-08-21 17:28:51 -0300

    User style

--- src/components/ProductsPagination.vue
+++ src/components/ProductsPagination.vue
@@ -63,7 +63,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .pagination {
 	grid-column: 1 / -1;
 }
--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -15,7 +15,7 @@
 					<router-link :to="{name: 'user-edit'}">Editar usuário</router-link>
 				</li>
 				<li>
-					<button @click.prevent="logout" class="btn">Sair</button>
+					<button @click="logout">Sair</button>
 				</li>
 			</ul>
 		</nav>
@@ -38,6 +38,46 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.user {
+	display: grid;
+	grid-template-columns: minmax(140px, 200px) 1fr;
+	grid-gap: 30px;
+	max-width: 900px;
+	margin: 40px auto;
+	padding: 20px;
+}
 
+@media screen and (max-width: 500px) {
+	.user {
+		grid-template-columns: 1fr;
+		margin: 0 auto;
+	}
+}
+
+.sidenav a,
+.sidenav button {
+	padding: 10px;
+	display: block;
+	color: #345;
+	background: #f4f7fc;
+	margin-bottom: 10px;
+	border-radius: 4px;
+}
+
+.sidenav a.router-link-exact-active,
+.sidenav a:hover,
+.sidenav button:hover {
+	background: #87f;
+	color: #fff;
+}
+
+.sidenav button {
+	border: none;
+	width: 100%;
+	font-size: 1rem;
+	text-align: left;
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	cursor: pointer;
+}
 </style>
